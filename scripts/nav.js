4c4a1769abd9eed48cfa18982232318e04dc10da
bompi <bompispam@gmail.com>
2024-06-08 18:39:04 +0200

    better navigation

--- scripts/nav.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    fetch('./elements/navigation.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('nav-container').innerHTML = data;
-        });
-}
